c5cfd2ed342881dcbe261599d8c39a52f71606fd
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-05-08 16:25:44 +0600

    Create COOK144-NETFLIX.java

---
+++ COOK144-NETFLIX.java
@@ -0,0 +1,33 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim = new Scanner(System.in);
+		int T = timtim.nextInt();
+		while(T-->0)
+		{
+		    int A = timtim.nextInt();
+		    int B = timtim.nextInt();
+		    int C = timtim.nextInt();
+		    int X = timtim.nextInt();
+		    
+		    if((A+B)>=X || (A+C)>=X || (B+C)>=X)
+		    {
+		        System.out.println("YES");
+		    }
+		    else
+		    {
+		        System.out.println("NO");
+		    }
+		    
+		}
+	}
+}
