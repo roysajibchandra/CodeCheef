3a0526b2d72fe09a7a16854af6dcf9db85387763
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-05-09 13:44:33 +0600

    Create START79-BETDEAL.java

---
+++ START79-BETDEAL.java
@@ -0,0 +1,34 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim = new Scanner(System.in);
+		int T = timtim.nextInt();
+		while(T-->0)
+		{
+		    int A = timtim.nextInt();
+		    int B = timtim.nextInt();
+		    
+		    if((100-((100*A)/100)) == ((200-(200*B)/100)))
+		    {
+		        System.out.println("BOTH");
+		    }
+		    else if ((100-((100*A)/100)) < ((200-(200*B)/100)))
+		    {
+		        System.out.println("FIRST");
+		    }
+		    else
+		    {
+		        System.out.println("SECOND");
+		    }
+		}
+	}
+}
