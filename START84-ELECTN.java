033f00b07cdcb9cdeab71b776611451ae3b48334
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-05-09 14:12:38 +0600

    Create START84-ELECTN.java

---
+++ START84-ELECTN.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim = new Scanner(System.in);
+		int T = timtim.nextInt();
+		while(T-->0)
+		{
+		    int N = timtim.nextInt();
+		    int X = timtim.nextInt();
+		    int count= 0;
+		    for(int i = 0; i<N; i++)
+		    {
+		        if(timtim.nextInt()>=X)
+		        {
+		            count++;
+		        }
+		    }
+		    System.out.println(count);
+		}
+	}
+}
