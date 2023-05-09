e6308ae87d5471f77fd104773cbd88b9a33d7540
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-05-10 00:14:48 +0600

    Create START87-EXISTENCE.java

---
+++ START87-EXISTENCE.java
@@ -0,0 +1,33 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim = new Scanner(System.in);
+		int T = timtim.nextInt();
+		while(T-->0)
+		{
+		    long X = timtim.nextLong();
+		    long Y = timtim.nextLong();
+		    
+		    long A = X*X*X*X + 4 * Y*Y;
+		    long B = 4 * X * X * Y;
+		    
+		    if(A==B)
+		    {
+		        System.out.println("YES");
+		    }
+		    else
+		    {
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
