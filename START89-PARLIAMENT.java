b8a293ce34ba2e334ef634ca125b34f346a3851d
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-05-15 03:04:01 +0600

    Create START89-PARLIAMENT.java

---
+++ START89-PARLIAMENT.java
@@ -0,0 +1,29 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim = new Scanner(System.in);
+		int T = timtim.nextInt();
+		do{
+		    int N = timtim.nextInt();
+		    int X = timtim.nextInt();
+		    
+		    if((N-X)<=X)
+		    {
+		        System.out.println("YES");
+		    }
+		    else
+		    {
+		        System.out.println("NO");
+		    }
+		}while(--T>0);
+	}
+}
