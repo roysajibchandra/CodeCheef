ed37ab619cb953afe9ffb74d4b886e7e14d96569
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-05-15 19:36:06 +0600

    Create START67-MINNOO.java

---
+++ START67-MINNOO.java
@@ -0,0 +1,48 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim = new Scanner(System.in);
+		int T = timtim.nextInt();
+		do{
+		    int N = timtim.nextInt();
+		    System.out.println(N/2);
+		}while(--T>0);
+    
+    /*
+    while(T-->0)
+		{
+		 int count=0;
+		 
+		int a = sc.nextInt();
+		String b = "";
+		 for(int i =0;i<a;i++)
+		 if(i%2==0)
+		 {
+		     b=b+"1";
+		     count++;
+		 }
+		 else{
+		     b=b+"0";
+		 }
+// 		 System.out.println(b);
+        if(a%2==0)
+        {
+		 System.out.println(count);
+        }
+        else{
+            System.out.println(count-1);
+        }
+		}
+    */
+    
+	}
+}
