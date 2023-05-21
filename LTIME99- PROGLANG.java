23a5f97668ebd6c2a8656d3b119547f62ac1c63a
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-05-21 09:01:09 +0600

    LTIME99- PROGLANG.java

---
+++ LTIME99- PROGLANG.java
@@ -0,0 +1,37 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim = new Scanner(System.in);
+		int T = timtim.nextInt();
+		while(T-->0)
+		{
+		    int A = timtim.nextInt();
+		    int B = timtim.nextInt();
+		    int A1 = timtim.nextInt();
+		    int B1 = timtim.nextInt();
+		    int A2 = timtim.nextInt();
+		    int B2 = timtim.nextInt();
+		    if((A==A1 || A==B1) && (B==A1 || B==B1))
+		    {
+		        System.out.println(1);
+		    }
+		    else if((A==A2 || A==B2) && (B==A2 || B==B2))
+		    {
+		        System.out.println(2);
+		    }
+		    else
+		    {
+		        System.out.println(0);
+		    }
+		}
+	}
+}
