425ec152ae289cf938184e1357afaae0d5e8bb92
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-05-26 12:32:07 +0600

    Min_SubArray_Sum.java

---
+++ Min_SubArray_Sum.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void MaxsubarrySum(int arr[])
+	{
+		int current = 0;
+		int Maxsum = Integer.MIN_VALUE;
+		int Minsum = Integer.MAX_VALUE;
+
+		for(int i=0; i<arr.length; i++)
+		{
+			int start = i;
+			for(int j=i; j<arr.length; j++)
+			{
+				int end = j;
+				current = 0;
+				for(int k=start; k<=end; k++)
+				{
+					System.out.print(arr[k]+" ");
+					current += arr[k];
+				}
+				System.out.println();
+				System.out.println("Sum is: " + current);
+				if(Minsum > current)
+				{
+					Minsum = current;
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("Max Sum is: " + Minsum);
+	}
+	
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner timtim = new Scanner(System.in);
+		int T = timtim.nextInt();
+		int arr[] = new int[T];
+		for(int i=0; i<T; i++)
+		{
+			arr[i] = timtim.nextInt();
+		}
+		System.out.println();
+		for(int i=0; i<arr.length; i++)
+		{
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+		MaxsubarrySum(arr);
+
+	}
+}
