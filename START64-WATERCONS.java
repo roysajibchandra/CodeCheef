990900de58a41d0c51a11848c863d7a5df1f85f3
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-05-28 00:02:37 +0600

    START64-WATERCONS.java

---
+++ START64-WATERCONS.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim = new Scanner(System.in);
+		int T = timtim.nextInt();
+		do{
+		    int X = timtim.nextInt();
+		    if(X>=2000)
+		    {
+		        System.out.println("YES");
+		    }
+		    else
+		    {
+		        System.out.println("NO");
+		    }
+		}while(--T>0);
+	}
+}
