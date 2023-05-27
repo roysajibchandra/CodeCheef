8a81f537a98ac5a67a40b76fbd1068e30bdfff5f
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-05-28 00:16:35 +0600

    START72-SUMM.java

---
+++ START72-SUMM.java
@@ -0,0 +1,20 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim = new Scanner(System.in);
+		int T = timtim.nextInt();
+		do{
+		    int A = timtim.nextInt(), B = timtim.nextInt(), C = timtim.nextInt();
+		    System.out.println((A+B)==C? "YES": "NO");
+		}while(--T!=0);
+	}
+}
