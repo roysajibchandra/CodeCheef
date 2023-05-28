f3f625a57f7590788148c8ff9f9568af626ccea9
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-05-28 12:46:05 +0600

    START73-LUCKYN.java

---
+++ START73-LUCKYN.java
@@ -0,0 +1,46 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim = new Scanner(System.in);
+		int T = timtim.nextInt();
+		while(T-->0)
+		{
+		  //  int X = timtim.nextInt();
+		    String Y = timtim.next();
+		    System.out.println(Y.contains("7")? "YES": "NO");
+		    
+		  //  boolean flag = false;
+		  //  while(X!=0)
+		  //  {
+		  //      int rem = X % 10;
+		        
+		  //      if(rem==7)
+		  //      {
+		  //          flag = true;
+		  //          break;
+		  //      }
+		        
+		  //       X = X/10;
+		  //  }
+		  //  if(flag)
+		  //  {
+		  //      System.out.println("YES");
+		  //  }
+		  //  else
+		  //  {
+		  //      System.out.println("NO");
+		  //  }
+		    
+		    
+		}
+	}
+}
