0343959253dccd0f401872c6bf1706497fd0cd05
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-05-28 13:01:19 +0600

    START36-PRESENTS.java

---
+++ START36-PRESENTS.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim = new Scanner(System.in);
+		int T = timtim.nextInt();
+		while(T-->0)
+		{
+		    int N = timtim.nextInt();
+		    if(N/5>=0)
+		    {
+		        System.out.println(N-(N/5));
+		    }
+		    else{
+		        System.out.println(N);
+		    }
+		}
+	}
+}
