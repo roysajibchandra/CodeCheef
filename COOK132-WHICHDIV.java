86fa12b05876c1624d3a3631bcd3344843c8be5a
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-06-05 17:53:18 +0600

    COOK132-WHICHDIV.java

---
+++ COOK132-WHICHDIV.java
@@ -0,0 +1,32 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim = new Scanner(System.in);
+		int T = timtim.nextInt();
+		while(T-->0)
+		{
+		    int R = timtim.nextInt();
+		    if(2000<=R)
+		    {
+		        System.out.println(1);
+		    }
+		    else if(1600<=R)
+		    {
+		        System.out.println(2);
+		    }
+		    else
+		    {
+		        System.out.println(3);
+		    }
+		}
+	}
+}
