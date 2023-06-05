d824b4da2a595275b258c52aa8f0941dcd8c0e5a
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-06-05 11:49:11 +0600

    APRIL21-SOCKS1.java

---
+++ APRIL21-SOCKS1.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim  = new Scanner(System.in);
+		int A = timtim.nextInt();
+		int B = timtim.nextInt();
+		int C = timtim.nextInt();
+		
+		if(A==B || A==C || B==C)
+		{
+		    System.out.println("YES");
+		}
+		else
+		{
+		    System.out.println("NO");
+		}
+	}
+}
