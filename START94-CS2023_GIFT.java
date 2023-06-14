dac7446033ceb055df922c67ce344d42529c5d4f
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-06-14 23:41:53 +0600

    START94-CS2023_GIFT.java

---
+++ START94-CS2023_GIFT.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim = new Scanner(System.in);
+		int X = timtim.nextInt();
+		int N = timtim.nextInt();
+		int M = timtim.nextInt();
+		
+		System.out.println((X+M)>=N? "YES": "NO");
+		
+// 		if(X+M >= N)
+// 		{
+// 		    System.out.println("YES");
+// 		}
+// 		else
+// 		{
+// 		    System.out.println("NO");
+// 		}
+	}
+}
+
