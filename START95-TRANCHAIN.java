1cd841ce5da22a2295de3ded8f93ab6e4152cfdf
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-06-26 01:55:33 +0600

    START95-TRANCHAIN.java

---
+++ START95-TRANCHAIN.java
@@ -0,0 +1,44 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim = new Scanner(System.in);
+		int T = timtim.nextInt();
+		while(T-->0)
+		{
+		    int N = timtim.nextInt();
+		    int A=0, B=0, O=0, AB=0;
+		    for(int i=0; i<N; i++)
+		    {
+		        String blood = timtim.next();
+		        if(blood.equals("A"))
+		        {
+		            ++A;
+		        }
+		        else if(blood.equals("B"))
+		        {
+		            ++B;
+		        }
+		        else if(blood.equals("O"))
+		        {
+		            ++O;
+		        }
+		        else
+		        {
+		            ++AB;
+		        }
+		    }
+		    int max = Math.max(A, B);
+		    max+=O+AB;
+		    System.out.println(max);
+		}
+	}
+}
