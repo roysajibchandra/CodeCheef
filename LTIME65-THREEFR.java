590af58236485ac5e7e51597d8dd0bf17795b112
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-08-10 17:14:52 +0600

    LTIME65-THREEFR.java

---
+++ LTIME65-THREEFR.java
@@ -0,0 +1,31 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim = new Scanner(System.in);
+		int T = timtim.nextInt();
+		while(T-->0)
+		{
+		    int A = timtim.nextInt();
+		    int B = timtim.nextInt();
+		    int C = timtim.nextInt();
+		    
+		    if( A+B==C || B+C==A || C+A==B )
+		    {
+		        System.out.println("yes");
+		    }
+		    else
+		    {
+		        System.out.println("no");
+		    }
+		}
+	}
+}
