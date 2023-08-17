0b0ce6a6a1a1e6c98fb148f2f7b323e4604350be
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-08-17 14:38:25 +0600

    START15-SUBTASK.java

---
+++ START15-SUBTASK.java
@@ -0,0 +1,58 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    public static Scanner timtim = new Scanner(System.in);
+    
+    public static void solve()
+    {
+        int T = timtim.nextInt();
+		while(T-->0)
+		{
+		    int N = timtim.nextInt();
+		    int M = timtim.nextInt();
+		    int K = timtim.nextInt();
+		    int[] Ai = new int[N];
+		    int count=0;
+		    for(int k=0; k<Ai.length; k++)
+		    {
+		        Ai[k] = timtim.nextInt();
+		        
+		    }
+		    for(int i=0; i<Ai.length; i++)
+		    {
+		        if(Ai[i]==1)
+		        {
+		            count++;
+		        }
+		        else
+		        {
+		            break;
+		        }
+		    }
+		    if(count==N)
+		    {
+		        System.out.println(100);
+		    }
+		    else if(count>=M)
+		    {
+		        System.out.println(K);
+		    }
+		    else
+		    {
+		        System.out.println(0);
+		    }
+		}
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		solve();
+	}
+}
